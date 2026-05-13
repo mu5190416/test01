e522f03927b3e7be8974c58c06079cf70c0f86f0
mu5190416 <tea061101@gmail.com>
2026-05-13 10:37:52 +0800

    更新 src/constants.ts via EZPage

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+import { Bento } from './types';
+
+export const BENTO_MENU: Bento[] = [
+  { id: '1', name: '打拋豬便當', price: 105 },
+  { id: '2', name: '日日招牌便當', price: 105 },
+  { id: '3', name: '秘製控肉便當', price: 125 },
+  { id: '4', name: '滷香排骨便當', price: 125 },
+  { id: '5', name: '特大滷雞腿便當', price: 135 },
+  { id: '6', name: '精選菜便當', price: 90 },
+  { id: '7', name: '特上叉燒便當', price: 105 },
+  { id: '8', name: '卡拉脆雞便當', price: 115 },
+  { id: '9', name: '日式豬排便當', price: 130 },
+  { id: '10', name: '黃金排骨便當', price: 125 },
+  { id: '11', name: '酥炸鯛魚便當', price: 130 },
+  { id: '12', name: '酥炸鯖魚便當', price: 170 },
+  { id: '13', name: '酥炸大雞腿便當', price: 135 },
+  { id: '14', name: '蝦捲便當', price: 130 },
+  { id: '15', name: '綜合便當', price: 115 },
+  { id: '16', name: '土魠魚便當', price: 130 },
+  { id: '17', name: '唐揚雞便當', price: 130 },
+];
